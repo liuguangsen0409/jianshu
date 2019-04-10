fe770576ea29c6a9250aee078eb99204a7fbcae2
liuguangsen0409 <865688540@qq.com>
2019-04-10 17:39:47 +0800

    detail页 布局 及异步获取文章标题内容

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { DetailWrapper, Header, Content } from './style'
+import { actionCreators } from './store'
 
-export default class Detail extends Component {
+class Detail extends Component {
+  componentDidMount () {
+    this.props.getDetail()
+  }
+
   render () {
+    const { title, content } = this.props
     return (
-      <div>detail</div>
+      <DetailWrapper>
+        <Header>{ title }</Header>
+        {/* 防止被转义 content 的内容应该这么取 */}
+        <Content dangerouslySetInnerHTML={{ __html: content }} />
+      </DetailWrapper>
     )
   }
 }
+
+const mapState = (state) => ({
+  title: state.getIn(['detail', 'title']),
+  content: state.getIn(['detail', 'content']),
+})
+
+const mapDispatch = (dispatch) => ({
+  getDetail () {
+    dispatch(actionCreators.getDetail())
+  }
+})
+
+export default connect(mapState, mapDispatch)(Detail)
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,12 @@
 import  { combineReducers } from 'redux-immutable'
 import { reducer as headerReducer } from '../common/header/store'
 import { reducer as homeReducer } from '../pages/home/store'
+import { reducer as detailReducer } from '../pages/detail/store'
 
 // 把很多小的 reducer 合并成一个大的reducer 提高代码的可维护性
 // 把 state 也变成一个 immutable 对象 在操作时 使用 get set 方法
 export default combineReducers({
   header: headerReducer,
-  home: homeReducer
+  home: homeReducer,
+  detail: detailReducer
 })
